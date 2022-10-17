77a67fdef5cf24280061e849baa8c66e26388bbe
A Park <atpark333@gmail.com>
2022-10-17 20:33:49 +0900

    Add tests and implementations

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const multipleOf = (x) => (n) => n % x == 0;
+
 module.exports = (num) => {
-  if (num % 15 == 0) {
+  if (multipleOf(15)(num)) {
     return "FizzBuzz";
   }
+  if (multipleOf(3)(num)) {
+    return "Fizz";
+  }
+  if (multipleOf(5)(num)) {
+    return "Buzz";
+  }
+
   return `${num}`;
 };
--- index.test.js
+++ index.test.js
@@ -10,4 +10,16 @@ describe('fizzBuzz()', () => {
     expect(fizzBuzz(4)).toBe('4');
     expect(fizzBuzz(71)).toBe('71');
   });
+
+  it('returns "Fizz" for multiples of 3', () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
+    expect(fizzBuzz(9)).toBe('Fizz');
+    expect(fizzBuzz(27)).toBe('Fizz');
+  });
+
+  it('returns "Buzz" for multiples of 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+    expect(fizzBuzz(25)).toBe('Buzz');
+    expect(fizzBuzz(55)).toBe('Buzz');
+  });
 });
